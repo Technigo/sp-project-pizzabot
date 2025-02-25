acd1b64946b432877d4c349d925118e6ae1abf28
annamost <anna-ostenrath@t-online.de>
2025-02-25 09:51:28 +0100

    update - added step3

--- code/script.js
+++ code/script.js
@@ -3,7 +3,7 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to the Chaos Pizzeria. Ready to Order? - Click 'OK' to begin.`
+  `Welcome to the Chaos Pizzeria.\nReady to Order? - Click 'OK' to begin.`
 )
 let name = prompt('Please enter your name here', '')
 
@@ -13,21 +13,42 @@ alert(
 
 // Step 2 - Food choice
 // Your code goes here
-let foodChoice = prompt('What would you like to order?\n1 - Pizza\n2 - Pasta\n3 - Desert', '')
+let foodChoice = prompt('What would you like to order? (enter a number) \n1 - Pizza\n2 - Pasta\n3 - Desert', '')
 
-if (foodChoice == 1) {
-  alert('You chose Pizza!')
-} else if (foodChoice == 2) {
-  alert('You chose Pasta!')
-} else if (foodChoice == 3) {
-  alert('You chose Desert!')
-} else {
-  alert('Invalid input, please pick a number between 1 and 3.')
-}
+const foodAvailable = ['Pizza', 'Pasta', 'Desert']
+
+alert(
+  `Yay! You chose ${foodAvailable[Number(foodChoice) - 1]}!`
+)
 
 
 // Step 3 - Subtype choice
+const pizzaChoice = ['Magherita', 'Parma', 'Vegeteriano', 'Bufalata']
+const pastaChoice = ['Ravioli', 'Spaghetti alla Carbonara', 'Linguine', 'Chef choice']
+const desertChoice = ['Tiramisu', 'Gelato', 'Panna cotta', 'Chocolate fondant']
+
 // Your code goes here
+if (foodChoice === '1') {
+  let subFood = prompt('Select a Pizza type (enter a number): \n1 - Magherita\n2 - Parma\n3 - Vegeteriano\n4 - Bufalata', '')
+
+  alert(
+    `You chose ${pizzaChoice[Number(subFood) - 1]}!`
+  )
+} else if (foodChoice === '2') {
+  let subFood = prompt('Select a Pasta type (enter a number): \n1 - Ravioli\n2 - Spaghetti alla Carbonara\n3 - Linguine\n4 - Chef choice', '')
+
+  alert(
+    `You chose ${pastaChoice[Number(subFood) - 1]}!`
+  )
+} else if (foodChoice === '3') {
+  let subFood = prompt('Select a Desert type (enter a number): \n1 - Tiramisu\n2 - Gelato\n3 - Panna cotta\n4 - Chocolate fondant', '')
+
+  alert(
+    `You chose ${desertChoice[Number(subFood) - 1]}!`
+  )
+}
+
+
 
 // Step 4 - Age
 // Your code goes here
